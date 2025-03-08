e62a17bb504f7b80bd3b858a49601a0a9351490b
Dreagonmon <531486058@qq.com>
2025-03-08 18:43:06 +0800

    changes to fit new gba-mpy-tools

---
+++ build_script.py
@@ -0,0 +1,9 @@
+from gba_mpy_tools.config import Config
+
+def before_build(cfg: Config):
+    # print("before_build")
+    pass
+
+def after_build(cfg: Config):
+    # print("after_build")
+    pass
